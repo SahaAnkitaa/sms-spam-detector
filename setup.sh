37d7e90ae213195bb68c15a03b0c1a03c4d9915f
SahaAnkitaa <118617672+SahaAnkitaa@users.noreply.github.com>
2024-08-30 13:51:47 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,3 +7,10 @@ enableCORS = false\n\
 headless = true\n\
 \n\
 " > ~/.streamlit/config.toml
+
+#updated
+# Create the nltk_data directory
+mkdir -p /app/nltk_data
+
+# Download NLTK data files
+python -c "import nltk; nltk.download('punkt', download_dir='/app/nltk_data'); nltk.download('stopwords', download_dir='/app/nltk_data')"
